2c7743bcc626aec589414ea1189bcd1959674aa7
John Hinnegan <john.hinnegan@gmail.com>
2011-02-21 17:42:25 -0500

    generating a consumer key

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :oauth_clients
+  map.resource :user_session
+  map.root :controller => "user_sessions", :action => "new" # optional, this just sets the root route
+  map.resource :account, :controller => "users"
+  map.resources :users
 
   map.test_request '/oauth/test_request', :controller => 'oauth', :action => 'test_request'
   map.access_token '/oauth/access_token', :controller => 'oauth', :action => 'access_token'
   map.request_token '/oauth/request_token', :controller => 'oauth', :action => 'request_token'
   map.authorize '/oauth/authorize', :controller => 'oauth', :action => 'authorize'
-  map.oauth '/oauth', :controller => 'oauth', :action => 'index'
+  map.oauth '/oauth', :controller => 'oauth_clients', :action => 'index'
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
